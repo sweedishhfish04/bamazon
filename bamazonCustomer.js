4ed770b01394923cf2d2391c29de118dd924d18f
sweedishhfish04 <godziwa4@yahoo.com>
2018-07-30 19:14:53 -0500

    getting the prompt to work correctly

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,14 +18,18 @@ connection.query('SELECT * FROM products', function (err, results, fields) {
     prompt.start();
 
     prompt.get(['id', 'units'], function (err, result) {
-      //  console.log(data,results)
-        var item = data.find(item => { return item.item_id === result.id })
-        if (result.units <= item.stock_quantity) {
-            connection.query(`UPDATE products SET stock_quantity ${item.stock_quantity - result.units} WHERE item_id = ${item.item_id}`, function (error, results, fields) {
+        //  console.log(data,results)
+        var item = data.find(itm => {
+         //   console.log(itm.item_id, result.id)
+            return itm.item_id === parseInt(result.id)
+        })
+        console.log(item);
+        if (parseInt(result.units) <= item.stock_quantity) {
+            connection.query(`UPDATE products SET stock_quantity ${item.stock_quantity - parseInt(result.units)} WHERE item_id = ${item.item_id}`, function (error, results, fields) {
                 if (err) {
                     console.error('Error making order')
                 } else {
-                    console.log(`Total Cost: ${result.units * item.price}`)
+                    console.log(`Total Cost: ${parseInt(result.units) * item.price}`)
                 }
             })
         } else {
